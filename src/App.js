9a6fbb7b87b8a1769abb383ad2bfc66916c9a71d
Alejo <alejobianchi@gmail.com>
2019-06-20 20:29:43 -0300

    Add new icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 /* global chrome */
 import React, { Component } from "react";
-import lampara16 from "./lampara-16.png";
+import watermelon16 from "./watermelon-16.png";
+import watermelon24 from "./watermelon-24.png";
+import watermelon32 from "./watermelon-32.png";
+import watermelon64 from "./watermelon-64.png";
+import watermelon128 from "./watermelon-128.png";
 import NewTab from "./Components/NewTab";
 
 class App extends Component {
